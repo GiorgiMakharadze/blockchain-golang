8967b1c7f860ebd2081fc1e1ac1bd0cee87ec611
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2024-07-22 01:00:59 +0400

    server star

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/GiorgiMakharadze/modular-blockchain-golang/network"
+)
 
 func main() {
-	fmt.Println("hello")
+	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+
+	go func() {
+		for {
+
+			trRemote.SendMessage(trLocal.Addr(), []byte("Hello world"))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	opts := network.ServerOpts{
+		Transports: []network.Transport{trLocal},
+	}
+
+	s := network.NewServer(opts)
+	s.Start()
 }
