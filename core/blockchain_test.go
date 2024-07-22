43589c7faf5c7c3b35496ba68b6a7ea311779cd1
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2024-07-23 01:52:41 +0400

    started additing  blockchain logic

---
+++ core/blockchain_test.go
@@ -0,0 +1,17 @@
+package core
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBlockchain(t *testing.T) {
+	bc, err := NewBlockchain(randomBlock(0))
+	assert.Nil(t, err)
+	assert.NotNil(t, bc.validator)
+
+	fmt.Println(bc.Height())
+
+}
